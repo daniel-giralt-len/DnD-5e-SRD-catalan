1476fc2f1e30019eae58abed4992874143dc5634
daniel-giralt-len <daniel.giralt.len@gmail.com>
2022-08-08 14:31:48 +0200

    no index anchor decoration on print

--- src/PrintLayout/DocumentIndex.js
+++ src/PrintLayout/DocumentIndex.js
@@ -1,8 +1,17 @@
 import styled from 'styled-components'
 
+const IndexElement = styled.p`
+    margin: 0.5em 0;
+`
+
 const SectionWrapper = styled.div`
     @media print {
         break-after: always;
+        color:white;
+        a {
+            color: black;
+            text-decoration: none;
+        }
     }
 `
 
@@ -12,7 +21,7 @@ const DocumentIndex = ({indexableSections}) => (
         {
             Object
                 .entries(indexableSections)
-                .map(([name, href]) => (<p key={name}><a href={`#${href}`}>{name}</a></p>))
+                .map(([name, href]) => (<IndexElement key={name}><a href={`#${href}`}>{name}</a></IndexElement>))
         }
     </SectionWrapper>
 )
