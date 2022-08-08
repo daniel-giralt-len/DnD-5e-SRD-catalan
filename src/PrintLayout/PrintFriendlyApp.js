415b454d79bd0e458944f22c68e247f534ef7781
daniel-giralt-len <daniel.giralt.len@gmail.com>
2022-08-08 17:55:27 +0200

    create boilerplate ClassSection

--- src/PrintLayout/PrintFriendlyApp.js
+++ src/PrintLayout/PrintFriendlyApp.js
@@ -3,18 +3,31 @@ import PrintLegalInfo from './PrintLegalInfo'
 import GenericSection from './GenericSection'
 import RacesSection from './RacesSection'
 import DocumentIndex from './DocumentIndex'
+import ClassSection from './ClassSection'
 
 const PrintableDocument = styled.main`
     max-width: 1200px;
     @media print {
         font-size:0.5em;
-        max-width: 100%
+        max-width: 100%;
     }
 `
 
 const sectionNameToHrefId = {
     'Legal Info': 'legal info',
     'Races': 'races',
+    'Bàrbar': 'barbar',
+    'Bard': 'bard',
+    'Brivall': 'brivall',
+    'Bruixot': 'bruixot',
+    'Clergue': 'clergue',
+    'Druida': 'druida',
+    'Explorador': 'explorador',
+    'Guerrer': 'guerrer',
+    'Mag': 'mag',
+    'Monjo': 'monjo',
+    'Paladí': 'paladí',
+    'Sortiller': 'sortiller',
 }
 
 const PrintFriendlyApp = ({sections}) => {
@@ -30,6 +43,15 @@ const PrintFriendlyApp = ({sections}) => {
             hrefId={sectionNameToHrefId['Races']}
             RacialTraits={<GenericSection doesPageBreak={false} {...s['Trets Racials']} />}
         />
+        {
+            s['Classes'].entries
+                .map(classData => (
+                    <ClassSection
+                    key={classData.class.name}
+                    hrefId={sectionNameToHrefId[classData.class.name]}
+                    {...classData.class}
+                />))
+        }
     </PrintableDocument>)
 }
 export default PrintFriendlyApp
