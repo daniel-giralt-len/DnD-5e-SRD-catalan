62471f0dc0f160c385c424419da09811eeccaee2
daniel-giralt-len <daniel.giralt.len@gmail.com>
2022-08-08 10:01:58 +0200

    render race speed

--- src/PrintLayout/Entries/Race.js
+++ src/PrintLayout/Entries/Race.js
@@ -7,6 +7,14 @@ const AbilityScoreIncrease = styled.span`
     font-weight: bold;
 `
 
+const ParagraphTitleStyle = styled.span`
+    font-style: italic;
+`
+
+const ParagraphTitle = ({children}) => <ParagraphTitleStyle>{children}. </ParagraphTitleStyle>
+
+const RaceEntry = styled.p``
+
 const Bold = styled.span`font-weight: bold;`
 
 const abilityKeyToLabel = {
@@ -19,8 +27,8 @@ const abilityKeyToLabel = {
 }
 
 const AbilityArray = (abilities) => (
-    <div>
-        Bonus a les puntuacions de característica. 
+    <RaceEntry>
+        <ParagraphTitle>Bonus a les puntuacions de característica</ParagraphTitle>
         {Object.entries(abilities).map(([k,v]) => {
             if(k==='choose'){
                 return (<span key={k}>i reparteix <Bold>{v.count}</Bold> punts entre <Bold>{v.from.map(k=>abilityKeyToLabel[k]).join(', ')}</Bold></span>)
@@ -28,7 +36,14 @@ const AbilityArray = (abilities) => (
             return (<AbilityScoreIncrease key={k}>{abilityKeyToLabel[k]} {toSignedStr(v)}</AbilityScoreIncrease>)
         }).reduce((prev, curr) => [prev, ', ', curr])}
         .
-    </div>
+    </RaceEntry>
+)
+
+const SpeedEntry = ({value}) => (
+    <RaceEntry>
+        <ParagraphTitle>Velocitat</ParagraphTitle>
+        {value} peus.
+    </RaceEntry>
 )
 
 
@@ -37,12 +52,14 @@ const restToIgnore = ['page','source','srd','soundClip']
 const Race = ({
     name,
     ability,
+    speed,
     ...rest
 },i) => {
     return(
     <article key={i}>
         <h3>{name}</h3>
         {ability && <AbilityArray {...ability[0]}/>}
+        {speed && <SpeedEntry value={speed} />}
         {JSON.stringify(Object.keys(rest).filter(k=>!restToIgnore.includes(k)))}
     </article>
 )}
