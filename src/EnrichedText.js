79a8560ea1c128c0ace7ed0d81a1f5d858c45d6a
daniel-giralt-len <daniel.giralt.len@gmail.com>
2022-08-08 15:25:25 +0200

    hide {@links}

--- src/EnrichedText.js
+++ src/EnrichedText.js
@@ -1,6 +1,18 @@
+const linkBuilder = {}
+
+const buildHyperLink = (match, linkType, linkData) => {
+    const linkArguments = linkData.split('|')
+    if(!linkBuilder[linkType]){
+        console.warn('no builder for', linkType, '; arguments are', linkArguments)
+        return linkArguments[0]
+    }
+    
+    return match
+}
+
 const EnrichedText = ({children}) => (
     <>
-        {children}
+        {children.replace(/{@(.*?) (.*?)}/gi, buildHyperLink)}
     </>
 )
 
