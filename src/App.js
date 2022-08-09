4b74c0804d0aff582ec2a611a20855493e3e0a05
daniel-giralt-len <daniel.giralt.len@gmail.com>
2022-08-09 12:07:54 +0200

    introduce references to srd

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import srdSections from './srd.json'
+import srd from './srd.json'
 import PrintFriendlyApp from './PrintLayout/PrintFriendlyApp'
 import { createGlobalStyle } from 'styled-components'
 
@@ -29,7 +29,10 @@ function App() {
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"></link>
       <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200;0,500;0,700;0,900;1,200;1,500;1,700&display=swap" rel="stylesheet"></link>
       <GlobalStyle />
-      <PrintFriendlyApp sections={srdSections} />
+      <PrintFriendlyApp
+        sections={srd.sections}
+        references={srd.references}
+      />
     </>
   );
 }
