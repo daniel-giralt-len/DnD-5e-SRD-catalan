f2c48b9d4ca0d34a23738751c8edd9c654f59582
daniel-giralt-len <daniel.giralt.len@gmail.com>
2022-08-10 17:37:12 +0200

    Bestiary: AC, HP, Speed

--- src/PrintLayout/Entries/Bestiary.js
+++ src/PrintLayout/Entries/Bestiary.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
 import { capitalizeFirstLetter } from '../../textModifiers'
 import { creatureSizeLabel, alignmentLabel, femaleCreatureSizeLabel, getAlignmentLabel } from '../../translationLists'
+import GenericEntry from './GenericEntry'
+import ColonEntry from './ColonEntry'
 
 const Wrapper = styled.div`
     break-inside: avoid-column;
@@ -26,13 +28,50 @@ const getAlignmentText = as => {
     
 }
 
+const getACText = acs => {
+    return acs
+        .map(ac => {
+            if(typeof ac !== 'object') { return ac.toString() }
+            const from = ac.from ? `(${ac.from})` : ''
+            const condition = ac.condition ? `${ac.condition}` : ''
+            return`${ac.ac} ${from} ${condition}`
+        })
+        .join(', ')
+}
+
+const getHPText = ({average, formula}) => `${average} (${formula})`
+
+const getSpeedsText = s => {
+    return Object.entries(s)
+        .map(([k,v]) => {
+            const base = `${v.number || v} peus`
+            const c = v.condition || ''
+            if(k==='walk') return `${base} ${c}`
+            if(k==='swim') return `${base} de natació ${c}`
+            if(k==='fly' || k === 'volar') return `${base} de vol ${c}`
+            if(k==='canHover') return `pot flotar ${c}`
+            if(k==='climb') return `${base} d'escalada ${c}`
+            if(k==='burrow') return `${base} excavant ${c}`
+            console.info('no method for speed',k)
+        })
+        .join(', ')
+}
+
 const Bestiary = ({
     name,
-    type, size, alineament
+    type, size, alineament,
+    ac,
+    hp,
+    speed,
 }) => (
     <Wrapper>
         <h2>{name}</h2>
         <Italic>{capitalizeFirstLetter(getTypeText(type))} {getCreatureSizeText(type, size).toLowerCase()}, {getAlignmentText(alineament)}</Italic>
+        <div>
+            <ColonEntry name="Classe d'Armadura (CA)" entry={getACText(ac)} />
+            <ColonEntry name="Punts de Vida (PV)" entry={getHPText(hp)}/>
+            <ColonEntry name="Velocitat" entry={getSpeedsText(speed)} />
+        </div>
     </Wrapper>
     )
 
