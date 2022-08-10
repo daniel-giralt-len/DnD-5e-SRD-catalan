ca6cda5c5f7dbcf611d165d4d9fd9404a15d6959
daniel-giralt-len <daniel.giralt.len@gmail.com>
2022-08-10 12:39:29 +0200

    render all spell data except description

--- src/PrintLayout/Entries/Spell.js
+++ src/PrintLayout/Entries/Spell.js
@@ -1,12 +1,9 @@
 import styled from 'styled-components'
 import { schoolLabel } from '../../translationLists'
-import GenericEntry from './GenericEntry'
 import ColonEntry from './ColonEntry'
 
 const Wrapper = styled.div`
-    display: flex;
-    justify-content: center;
-    margin-top: 0.3em;
+    break-inside: avoid-column;
 `
 
 const getLevelText = lvl => lvl === 0 ? 'Truc' : `Nivell ${lvl}`
@@ -19,6 +16,21 @@ const getComponentsText = ({v,s,m}) => [
     m?`M (${m.text || m})`:''
 ].filter(i=>i).join(', ')
 
+const durationsPlurals = {
+    ronda: ['ronda','rondes'],
+    minut: ['minut','minuts'],
+    hora: ['hora','hores'],
+    dia: ['dia','dies'],
+}
+const lengthsPlurals = {
+    peus: ['peu','peus'],
+    milles: ['milla','milles']
+}
+
+const getPlural = (t,a,l) => a === 1 ? l[t][0] : l[t][1]
+const getDurationPlural = (t,a) => getPlural(t,a,durationsPlurals)
+const getLengthsPlural = (t,a) => getPlural(t,a,lengthsPlurals)
+
 const getDurationText = ({
     type,
     duration,
@@ -26,17 +38,8 @@ const getDurationText = ({
     ends
 }) => {
     if(type === 'cronometrada'){
-        let out = ''
-        if(concentration){ out += 'Concentració, fins a '}
-        out += `${duration.amount} `
-        const durationWords = {
-            ronda: ['ronda','rondes'],
-            minut: ['minut','minuts'],
-            hora: ['hora','hores'],
-            dia: ['dia','dies'],
-        }
-        if(duration.amount === 1){ out += durationWords[duration.type][0]}
-        else{ out += durationWords[duration.type][1]}
+        let out = `${duration.amount} ${getDurationPlural(duration.type, duration.amount)}`
+        if(concentration){ out = `Concentració, fins a ${out}`}
         return out
     }
     if(type === 'permanent'){
@@ -49,24 +52,61 @@ const getDurationText = ({
     console.info(type)
 }
 
+const getRangeText = ({
+    type,
+    distance
+}) => {
+    if(type === 'point') {
+        if(distance.type === 'tocar') { return 'Tocar' }
+        if(distance.type === 'peus') { return `${distance.amount} peus` }
+        if(distance.type === 'llançador') { return `Llançador/a/i` }
+        if(distance.type === 'a vista') { return `A vista` }
+        if(distance.type === 'unlimited') { return `Il·limitat` }
+        if(distance.type === 'milles') { return `${distance.amount} ${distance.amount === 1 ? 'milla' : 'milles'}`}
+    }
+    if(type === 'especial') { return 'Especial'}
+    const centered = '(Llançador/a/i)'
+    if(type === 'hemisphere') {
+        return `Hemisferi de ${distance.amount} ${getLengthsPlural( distance.type, distance.amount)} de radi ${centered}`
+    }
+    if(type === 'esfera') {
+        return `Esfera de ${distance.amount} ${getLengthsPlural( distance.type, distance.amount)} de radi ${centered}`
+    }
+    if(type === 'radius') {
+        return `${distance.amount} ${getLengthsPlural( distance.type, distance.amount)} de radi ${centered}`
+    }
+    if(type === 'line') {
+        return `Línia de ${distance.amount} ${getLengthsPlural( distance.type, distance.amount)} ${centered}`
+    }
+    if(type === 'cube') {
+        return `Cub de ${distance.amount} ${getLengthsPlural( distance.type, distance.amount)} de costat ${centered}`
+    }
+    if(type === 'con') {
+        return `Con de ${distance.amount} ${getLengthsPlural( distance.type, distance.amount)} ${centered}`
+    }
+    console.info('unknown spell range type',type)
+}
+
 const Spell = ({
     name,
+    srd,
     level,
     school,
     time,
     components,
-    duration
+    duration,
+    range
 }) => (
-    <div>
-        <h2>{name}</h2>
+    <Wrapper>
+        <h2>{typeof srd === 'string' ? srd : name}</h2>
         <div>
             <span>{getLevelText(level)}</span>, <span>{schoolLabel[school]}</span>
             <ColonEntry name='Duració de Llançament' entry={getCastingTimeText(time[0])} />
-            <ColonEntry name='Abast' />
+            <ColonEntry name='Abast' entry={getRangeText(range)} />
             <ColonEntry name='Components' entry={getComponentsText(components)} />
             <ColonEntry name='Duració' entry={getDurationText(duration[0])}/>
         </div>
-    </div>
+    </Wrapper>
     )
 
 export default Spell
